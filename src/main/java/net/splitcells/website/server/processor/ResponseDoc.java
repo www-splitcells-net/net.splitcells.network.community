9705be799c6e099b9aa26647c4570ee4d4ca325f
Mārtiņš Avots <martins.avots@splitcells.net>
2023-12-11 17:49:35 +0100

    Committed by "splitcells" on "splitcells-XPS-15-9570".

---
+++ src/main/java/net/splitcells/website/server/processor/ResponseDoc.java
@@ -0,0 +1,9 @@
+package net.splitcells.website.server.processor;
+
+/**
+ * <p>These are notes for {@link net.splitcells.website.server.processor.Response}.</p>
+ * <p>
+ * TODO Create a response type for forms, that makes it easier to add errors, field updates, etc. to the response.
+ */
+public class ResponseDoc {
+}
