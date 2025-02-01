e846bdebad9ee343da0ac35166aef284af568499
Mārtiņš Avots <martins.avots@splitcells.net>
2025-02-01 05:50:15 +0100

    Committed by "splitcells" on "splitcells-XPS-15-9570".

--- src/main/java/net/splitcells/network/community/NetworkCommunityFileSystem.java
+++ src/main/java/net/splitcells/network/community/NetworkCommunityFileSystem.java
@@ -15,12 +15,12 @@
  */
 package net.splitcells.network.community;
 
-import net.splitcells.dem.environment.config.framework.OptionI;
+import net.splitcells.dem.environment.config.framework.OptionImpl;
 import net.splitcells.dem.resource.FileSystemView;
 
 import static net.splitcells.dem.resource.FileSystemViaClassResources.fileSystemViaClassResources;
 
-public class NetworkCommunityFileSystem extends OptionI<FileSystemView> {
+public class NetworkCommunityFileSystem extends OptionImpl<FileSystemView> {
     public NetworkCommunityFileSystem() {
         super(() -> fileSystemViaClassResources(NetworkCommunityFileSystem.class
                 , "net.splitcells", "network.community"));
